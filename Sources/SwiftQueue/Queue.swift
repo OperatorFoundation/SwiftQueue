3ce25dc0043ad062a01d1ac1801cc97fb8ae5ad8
Lita <looklita@gmail.com>
2018-05-16 16:25:12 -0500

    Thread Safe

--- Sources/SwiftQueue/Queue.swift
+++ Sources/SwiftQueue/Queue.swift
@@ -6,19 +6,26 @@
 //  Courtesy of https://www.raywenderlich.com/148141/swift-algorithm-club-swift-queue-data-structure
 
 import Foundation
+import Dispatch
 
 public class Queue<T>
 {
     fileprivate var list = LinkedList<T>()
+    
     public var isEmpty: Bool { return list.isEmpty }
+    
+    private var lock = DispatchGroup.init()
     
     public func enqueue(_ element: T)
     {
+        lock.enter()
         list.append(value: element)
+        lock.leave()
     }
     
     public func dequeue() -> T?
     {
+        lock.enter()
         guard !list.isEmpty, let element = list.first
         else
         {
@@ -26,6 +33,8 @@ public class Queue<T>
         }
         
         let poppedValue = list.pop(node: element)
+        lock.leave()
+        
         return poppedValue
     }
     
