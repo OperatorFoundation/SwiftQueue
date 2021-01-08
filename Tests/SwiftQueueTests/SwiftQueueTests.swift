d5396752461d0db4e89192c43932921cd64f0209
consuelita <adelita@operatorfoundation.org>
2021-01-07 19:11:08 -0600

    Add gitignore

--- Tests/SwiftQueueTests/SwiftQueueTests.swift
+++ Tests/SwiftQueueTests/SwiftQueueTests.swift
@@ -6,7 +6,7 @@ final class SwiftQueueTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftQueue().text, "Hello, World!")
+        //XCTAssertEqual(SwiftQueue().text, "Hello, World!")
     }
 
 
